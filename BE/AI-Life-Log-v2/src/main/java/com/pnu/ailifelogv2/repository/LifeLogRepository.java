825057ecbfc2416755cb1e91a97b7df137501e39
Swallow Lee <lmh157@outlook.kr>
2025-06-05 03:48:18 +0900

    [#12 fix] ai repository와 일반 repository 분리(function calling을 위해)

--- BE/AI-Life-Log-v2/src/main/java/com/pnu/ailifelogv2/repository/LifeLogRepository.java
+++ BE/AI-Life-Log-v2/src/main/java/com/pnu/ailifelogv2/repository/LifeLogRepository.java
@@ -5,17 +5,14 @@ import com.pnu.ailifelogv2.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
 
+@Repository
 public interface LifeLogRepository extends JpaRepository<LifeLog, Long> {
     Page<LifeLog> findByUser(User user, Pageable pageable);
     Optional<LifeLog> findByIdAndUser(Long id, User user);
     Page<LifeLog> findByTimestampBetweenAndUser(LocalDateTime start, LocalDateTime end, User user, Pageable pageable);
-
-    // for ai search
-    Page<LifeLog> findByTitleContainingAndUser(String title, User user, Pageable pageable);
-    Page<LifeLog> findByDescriptionContainingAndUser(String description, User user, Pageable pageable);
-
 }
