23390b14b1d4a78515eb9602e21cf93f56abd15f
Swallow Lee <lmh157@outlook.kr>
2025-06-04 23:57:29 +0900

    [#8 feature] jwt 인가 관련 로직 구현
      -[#8 feature] 미인가 요청 핸들러 구현
      -[#8 feature] 인증 필터 구현
      -[#8 feature] jwt token 생성 로직 구현

---
+++ BE/AI-Life-Log-v2/src/main/java/com/pnu/ailifelogv2/component/jwt/TokenProvider.java
@@ -0,0 +1,89 @@
+package com.pnu.ailifelogv2.component.jwt;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class TokenProvider implements InitializingBean {
+    private static final String AUTHORITIES_KEY = "auth";
+
+    private final String secret;
+    private final Long expiration;
+    private SecretKey secretKey;
+
+
+    public TokenProvider(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.expiration}") Long expiration) {
+        this.secret = secret;
+        this.expiration = expiration;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // Initialize the secretKey using the secret value
+        secretKey = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String generateToken(Authentication authentication) {
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime expiredAt = now.plusSeconds(expiration);
+
+        String authorities = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        return io.jsonwebtoken.Jwts.builder()
+                .subject(authentication.getName())
+                .claim(AUTHORITIES_KEY, authorities)
+                .issuedAt(Date.from(now.atZone(ZoneId.systemDefault()).toInstant()))
+                .expiration(Date.from(expiredAt.atZone(ZoneId.systemDefault()).toInstant()))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public Authentication getAuthentication(String jwt) {
+        Claims claims = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(jwt).getPayload();
+        String username = claims.getSubject();
+        String authoritiesString = claims.get(AUTHORITIES_KEY).toString();
+
+        Collection<? extends GrantedAuthority> authorities =
+                Arrays.stream(authoritiesString.split(","))
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+        return new UsernamePasswordAuthenticationToken(username, "", authorities);
+    }
+
+    public boolean validateToken(String jwt) {
+        try {
+            Jwts.parser().verifyWith(secretKey).build().parse(jwt);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+}
